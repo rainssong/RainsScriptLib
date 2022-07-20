ec9c8972cb673b0206de560d3e1b9e3578b4db3a
rainssong <rainssong@outlook.com>
2022-07-20 21:02:48 +0800

    update

---
+++ src/结束进程.bat
@@ -0,0 +1,18 @@
+@echo off
+:start
+set /P C=[结束进程]: 
+if "%C%" == "" goto info
+setlocal enabledelayedexpansion
+tasklist /fi "pid eq %C%" | find "%C%" > nul && (
+  tasklist /fi "pid eq %C%"
+  echo.
+  set /p str="是否要结束 PID 为 %C% 的进程(Y/N)? "
+  if /I "!str!" == "Y" taskkill /f /pid %C%
+) || echo 没有找到 PID 为 %C% 的进程
+goto end
+:info
+echo 使用方法：kill ^<需要结束的进程号(PID)^>
+:end
+pause.
+goto start
+echo on
--- src/释放端口.bat
+++ src/释放端口.bat
@@ -1,15 +1,61 @@
 @echo off
-set /P C=[Choice]: 
-if "%C" == "" goto info
-setlocal enabledelayedexpansion
-tasklist /fi "pid eq %C" | find "%C" > nul && (
-  tasklist /fi "pid eq %C"
-  echo.
-  set /p str="是否要结束 PID 为 %1 的进程(Y/N)? "
-  if /I "!str!" == "Y" taskkill /f /pid %C
-) || echo 没有找到 PID 为 %C 的进程
-goto end
-:info
-echo 使用方法：kill ^<需要结束的进程号(PID)^>
-:end
-echo on
+cls
+color 0a
+TITLE KillPort Power By xx
+
+:main
+ECHO. ********************************************************************
+ECHO. * *
+ECHO. * *  1.全部端口列表
+ECHO. * *  2.查询端口信息
+ECHO. * *  3.查询PID对应进程
+ECHO. * *  4.解除端口占用
+ECHO. * *
+ECHO. ********************************************************************
+	echo.
+set /p X=输入数字选择功能:
+
+if %X%==1 (
+	goto findAllPort
+)else if %x%==2 (
+goto mPort
+)else if %x%==3 (
+goto findPid
+)else if %x%==4 (
+goto killPort
+)
+
+:findAllPort
+cls
+	echo.	
+	netstat -aon
+	echo.
+	goto main
+	
+:mPort
+cls
+    set /p mport=请输入要查找的端口:
+	echo.	
+	echo   协议   本地地址          	外部地址        	状态           PID
+	netstat -aon|findstr %mport%
+	echo.
+	goto main
+
+:findPid
+cls
+    set /p mPid=请输入端口对应的PID号:
+	tasklist|findstr %mPid%
+	echo.
+	goto main
+	
+:killPort
+cls
+set /p port=请输入端口号：
+for /f "tokens=1-5" %%i in ('netstat -ano^|findstr ":%port%"') do (
+    echo kill the process %%m who use the port %port%
+    tasklist|findstr %%m
+    echo.
+    taskkill /f /pid %%m
+)
+	echo.
+	goto main
